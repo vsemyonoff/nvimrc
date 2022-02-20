566f2123955213b8c0a6d31b5c628539e56054ed
Vladislav Semyonoff <vsemyonoff@gmail.com>
2022-02-20 20:25:15 +0200

    lualine: update settings

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -1,1 +1,22 @@
-require('lualine').setup()
+require('lualine').setup {
+    options = {
+        theme = 'onedark',
+        disabled_filetypes = { 'neo-tree' },
+        section_separators = '',
+        component_separators = '',
+    },
+    sections = {
+        lualine_a = { { 'mode', fmt = function(str) return str:sub(1, 1) end } },
+        lualine_b = { 'branch' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = {
+            {
+                'diagnostics',
+                always_visible = true,
+                sections = { 'error', 'warn' },
+            },
+        },
+        lualine_y = { { 'filetype', icon_only = false } },
+        lualine_z = { 'progress' },
+    },
+}
